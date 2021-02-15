7321ab9644162b31101a74ea212d834352a45869
sumathim85 <74769258+sumathim85@users.noreply.github.com>
2021-02-15 15:24:01 +0530

    Update UpdateScheduleDetails.java

--- UpdateScheduleDetails.java
+++ UpdateScheduleDetails.java
@@ -11,7 +11,7 @@ public interface UpdateScheduleDetails extends JpaRepository<Scheduler,String> {
     @Transactional
     @Modifying
         @Query(value="update scheduler s set s.month = :month,s.day_of_week = :day_of_week,s.day = :day,s.day = :day,s.minute = :minute,s.hour = :hour,s.request_body = :request_body where s.scheduler_id =:scheduler_id",nativeQuery = true)
-//    @Query(value="update scheduler s set s.month = 12 where s.scheduler_id =7",nativeQuery = true)
+//    @Query(value="update scheduler s set s.month = 12 where s.scheduler_id =7",nativeQuery = true,false)
     void updateSchedule(@Param("scheduler_id") Integer scheduler_id, @Param("month") String month,@Param("day_of_week") String day_of_week,@Param("day") String day,@Param("minute") String minute,@Param("hour") String hour,@Param("request_body") String request_body);
 
 }
